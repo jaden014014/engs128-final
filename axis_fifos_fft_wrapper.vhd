260c91032134e0b255b722bdd5e9b6e92461b841
Jaden M. Parker <f007xhp@dartmouth.edu>
2026-05-27 08:32:04 -0400

    feeder fsm done, debugging wrapper

--- axis_fifos_fft_wrapper.vhd
+++ axis_fifos_fft_wrapper.vhd
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------
---  Final Project: AXI Stream FFT and HDMI output
+--  Final Project: AXI Stream FFT and HDMI
 ----------------------------------------------------------------------------
 --  ENGS 128 
 --	Author: Jaden Parker
@@ -11,43 +11,44 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity axis_fft_wrapper is
+entity axis_fifos_fft_wrapper is
     generic (
-        IN_DATA_WIDTH  : integer := 32;   -- 24-bit audio inside a 32-bit word
-        OUT_DATA_WIDTH : integer := 64;   -- 32-bit Real + 32-bit Imaginary
-        FIFO_DEPTH     : integer := 1024; -- Depth for both FIFOs
-        CONFIG_WIDTH   : integer := 16    -- Default Xilinx FFT config width
+        DATA_WIDTH : integer := 32;  
+        FIFO_DEPTH     : integer := 64; 
+        CONFIG_WIDTH   : integer := 16; 
+        FFT_DATA_WIDTH : integer := 48  
     );
     port (
-        -- Global Clock and Reset
+
         aclk            : in  std_logic;
         aresetn         : in  std_logic;
+        lrclk_raw      : in  std_logic;
+        lrclk_bufg      : in  std_logic;
 
-        -- Slave Interface (Connects to your Upstream I2S/Audio Source)
         s_axis_tvalid   : in  std_logic;
-        s_axis_tdata    : in  std_logic_vector(IN_DATA_WIDTH-1 downto 0);
-        s_axis_tstrb    : in  std_logic_vector((IN_DATA_WIDTH/8)-1 downto 0);
+        s_axis_tdata    : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+        s_axis_tstrb    : in  std_logic_vector((DATA_WIDTH/8)-1 downto 0);
         s_axis_tlast    : in  std_logic;
         s_axis_tready   : out std_logic;
 
-        -- Master Interface (Connects to your Downstream DMA / Processor)
+
         m_axis_tvalid   : out std_logic;
-        m_axis_tdata    : out std_logic_vector(OUT_DATA_WIDTH-1 downto 0);
-        m_axis_tstrb    : out std_logic_vector((OUT_DATA_WIDTH/8)-1 downto 0);
+        m_axis_tdata    : out std_logic_vector(DATA_WIDTH-1 downto 0);
+        m_axis_tstrb    : out std_logic_vector((DATA_WIDTH/8)-1 downto 0);
         m_axis_tlast    : out std_logic;
         m_axis_tready   : in  std_logic
     );
-end axis_fft_wrapper;
+end axis_fifos_fft_wrapper;
 
-architecture Structural of axis_fft_wrapper is
+architecture Behavioral of axis_fifos_fft_wrapper is
 
 ------------------------------------------------------------------------
 -- Component Declarations
 ------------------------------------------------------------------------ 
 component axis_fifo is
     generic (
-        DATA_WIDTH : integer := 32;
-        FIFO_DEPTH : integer := 1024
+        DATA_WIDTH : integer := DATA_WIDTH;
+        FIFO_DEPTH : integer := FIFO_DEPTH
     );
     port (
         s00_axis_aclk    : in std_logic;
@@ -67,7 +68,26 @@ component axis_fifo is
         m00_axis_tready  : in std_logic
     );
 end component;
-I need fft_feeder_fsm
+
+component fft_feeder_fsm is
+	generic (
+		FIFO_DEPTH	: integer	:= 64;
+		FFT_DATA_WIDTH : integer	:= FFT_DATA_WIDTH
+	);    
+    Port ( 
+        --Config ports 
+        config_tdata_o : out std_logic_vector(15 downto 0);
+        config_tvalid_o : out std_logic;       
+        config_tready_i : in std_logic;  
+        
+        lrclk_bufg : in std_logic;
+        aclk     : in std_logic;
+		aresetn  : in std_logic;
+
+		tlast_o    : out std_logic
+    );
+end component;
+
 -- FFT IP 
 component axis_fft is
     port (
@@ -76,46 +96,97 @@ component axis_fft is
         s_axis_config_tdata  : in  std_logic_vector(CONFIG_WIDTH-1 downto 0);
         s_axis_config_tvalid : in  std_logic;
         s_axis_config_tready : out std_logic;
-        s_axis_data_tdata    : in  std_logic_vector(OUT_DATA_WIDTH-1 downto 0);
+        s_axis_data_tdata    : in  std_logic_vector(FFT_DATA_WIDTH-1 downto 0);
         s_axis_data_tvalid   : in  std_logic;
         s_axis_data_tready   : out std_logic;
         s_axis_data_tlast    : in  std_logic;
-        m_axis_data_tdata    : out std_logic_vector(OUT_DATA_WIDTH-1 downto 0);
+        m_axis_data_tdata    : out std_logic_vector(FFT_DATA_WIDTH-1 downto 0);
         m_axis_data_tvalid   : out std_logic;
         m_axis_data_tready   : in  std_logic;
+        m_axis_data_tuser    : out std_logic_vector(8-1 downto 0);
         m_axis_data_tlast    : out std_logic
     );
 end component;
 
--- Signals from Input FIFO to FFT
-signal fifo_in_m_tvalid  : std_logic;
-signal fifo_in_m_tdata   : std_logic_vector(IN_DATA_WIDTH-1 downto 0);
-signal fifo_in_m_tstrb   : std_logic_vector((IN_DATA_WIDTH/8)-1 downto 0);
-signal fifo_in_m_tlast   : std_logic;
-signal fifo_in_m_tready  : std_logic;
+component counter is
+    Generic ( MAX_COUNT : integer);   
+    Port (  clk_i       : in STD_LOGIC;			
+            reset_i     : in STD_LOGIC;		
+            enable_i    : in STD_LOGIC;				
+            tc_o        : out STD_LOGIC);
+end component counter;
+
+-- Signals from Input FIFO to L and R data fifos
+signal fifo_0_m_tvalid  : std_logic;
+signal fifo_0_m_tdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal fifo_0_m_tstrb   : std_logic_vector((DATA_WIDTH/8)-1 downto 0);
+signal fifo_0_m_tlast   : std_logic;
+signal fifo_0_m_tready  : std_logic;
+
+signal fifo_l_s_tvalid  : std_logic;
+signal fifo_l_s_tdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal fifo_l_s_tstrb   : std_logic_vector((DATA_WIDTH/8)-1 downto 0);
+signal fifo_l_s_tlast   : std_logic;
+signal fifo_l_s_tready  : std_logic;
+
+signal fifo_l_m_tvalid  : std_logic;
+signal fifo_l_m_tdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal fifo_l_m_tstrb   : std_logic_vector((DATA_WIDTH/8)-1 downto 0);
+signal fifo_l_m_tlast   : std_logic;
+signal fifo_l_m_tready  : std_logic;
+
+signal fifo_r_s_tvalid  : std_logic;
+signal fifo_r_s_tdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal fifo_r_s_tstrb   : std_logic_vector((DATA_WIDTH/8)-1 downto 0);
+signal fifo_r_s_tlast   : std_logic;
+signal fifo_r_s_tready  : std_logic;
+
+signal fifo_r_m_tvalid  : std_logic;
+signal fifo_r_m_tdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal fifo_r_m_tstrb   : std_logic_vector((DATA_WIDTH/8)-1 downto 0);
+signal fifo_r_m_tlast   : std_logic;
+signal fifo_r_m_tready  : std_logic;
+
 
 -- FFT Data Input
-signal fft_s_tdata       : std_logic_vector(OUT_DATA_WIDTH-1 downto 0);
-
+signal fft_s_tdata       : std_logic_vector(FFT_DATA_WIDTH-1 downto 0);
+signal fft_s_tvalid  : std_logic;
+signal fft_s_tready  : std_logic;
+signal fft_s_tlast  : std_logic;
 
 signal config_tvalid     : std_logic := '0';
 signal config_tready     : std_logic;
-signal config_done       : std_logic := '0';
+signal config_tdata     : std_logic_vector(15 downto 0);
 
 -- Signals from FFT  to output FIFO
 signal fft_m_tvalid      : std_logic;
-signal fft_m_tdata       : std_logic_vector(OUT_DATA_WIDTH-1 downto 0);
+signal fft_m_tdata       : std_logic_vector(DATA_WIDTH-1 downto 0);
 signal fft_m_tlast       : std_logic;
 signal fft_m_tready      : std_logic;
+
+
+signal lrclk_raw_sync1 : std_logic;
+signal lrclk_raw_sync2 : std_logic;
+
+signal lrclk_bufg_sync1 : std_logic;
+signal lrclk_bufg_sync2 : std_logic;
+
+signal tc_l : std_logic;
+signal tc_r : std_logic;
+signal reset_l : std_logic;
+signal reset_r : std_logic;
+
+type state_type is (LeftSelect, RightSelect);	
+signal curr_state, next_state : state_type := RightSelect;
 
 begin
 
 ------------------------------------------------------------------------
--- 1. Input FIFO Instantiation (32-bit Data Width)
+-- Fifo 0 goes to either fifo l or fifo r depending on lrclk
 ------------------------------------------------------------------------
 input_fifo_inst : axis_fifo
     generic map (
-        DATA_WIDTH => IN_DATA_WIDTH,
+        DATA_WIDTH => DATA_WIDTH,
         FIFO_DEPTH => FIFO_DEPTH
     )
     port map (
@@ -129,82 +200,124 @@ input_fifo_inst : axis_fifo
 
         m00_axis_aclk     => aclk,
         m00_axis_aresetn  => aresetn,
-        m00_axis_tvalid   => fifo_in_m_tvalid,
-        m00_axis_tdata    => fifo_in_m_tdata,
-        m00_axis_tstrb    => fifo_in_m_tstrb,
-        m00_axis_tlast    => fifo_in_m_tlast,
-        m00_axis_tready   => fifo_in_m_tready
+        m00_axis_tvalid   => fifo_0_m_tvalid,
+        m00_axis_tdata    => fifo_0_m_tdata,
+        m00_axis_tstrb    => fifo_0_m_tstrb,
+        m00_axis_tlast    => fifo_0_m_tlast,
+        m00_axis_tready   => fifo_0_m_tready
     );
 
-------------------------------------------------------------------------
--- 2. Data Bus Packing (Real Audio Data -> Complex FFT Format)
-------------------------------------------------------------------------
--- zeros for real Audio
-fft_s_tdata <= (63 downto 32 => '0') & fifo_in_m_tdata;
+input_fifo_left : axis_fifo
+    generic map (
+        DATA_WIDTH => DATA_WIDTH,
+        FIFO_DEPTH => FIFO_DEPTH
+    )
+    port map (
+        s00_axis_aclk     => aclk,
+        s00_axis_aresetn  => aresetn,
+        s00_axis_tvalid   => fifo_l_s_tvalid,
+        s00_axis_tdata    => fifo_l_s_tdata,
+        s00_axis_tstrb    => fifo_l_s_tstrb,
+        s00_axis_tlast    => fifo_l_s_tlast,
+        s00_axis_tready   => fifo_l_s_tready,
 
-------------------------------------------------------------------------
--- 3. Automated FFT Configuration State Machine
-------------------------------------------------------------------------
-process(aclk)
-begin
-    if rising_edge(aclk) then
-        if aresetn = '0' then
-            config_tvalid <= '0';
-            config_done   <= '0';
-        else
-            if config_done = '0' then
-                config_tvalid <= '1';
-                if config_tready = '1' and config_tvalid = '1' then
-                    config_tvalid <= '0';
-                    config_done   <= '1';
-                end if;
-            else
-                config_tvalid <= '0';
-            end if;
-        end if;
-    end if;
-end process;
+        m00_axis_aclk     => aclk,
+        m00_axis_aresetn  => aresetn,
+        m00_axis_tvalid   => fifo_l_m_tvalid,
+        m00_axis_tdata    => fifo_l_m_tdata,
+        m00_axis_tstrb    => fifo_l_m_tstrb,
+        m00_axis_tlast    => fifo_l_m_tlast,
+        m00_axis_tready   => fifo_l_m_tready
+    );
+    
+input_fifo_right : axis_fifo
+    generic map (
+        DATA_WIDTH => DATA_WIDTH,
+        FIFO_DEPTH => FIFO_DEPTH
+    )
+    port map (
+        s00_axis_aclk     => aclk,
+        s00_axis_aresetn  => aresetn,
+        s00_axis_tvalid   => fifo_r_s_tvalid,
+        s00_axis_tdata    => fifo_r_s_tdata,
+        s00_axis_tstrb    => fifo_r_s_tstrb,
+        s00_axis_tlast    => fifo_r_s_tlast,
+        s00_axis_tready   => fifo_r_s_tready,
 
-------------------------------------------------------------------------
--- 4. Xilinx FFT IP Core Instantiation
-------------------------------------------------------------------------
+        m00_axis_aclk     => aclk,
+        m00_axis_aresetn  => aresetn,
+        m00_axis_tvalid   => fifo_r_m_tvalid,
+        m00_axis_tdata    => fifo_r_m_tdata,
+        m00_axis_tstrb    => fifo_r_m_tstrb,
+        m00_axis_tlast    => fifo_r_m_tlast,
+        m00_axis_tready   => fifo_r_m_tready
+    );
+
+fft_feeder_inst: fft_feeder_fsm
+    generic map(
+    FIFO_DEPTH	=> FIFO_DEPTH,
+    FFT_DATA_WIDTH => DATA_WIDTH
+	)   
+    port map(
+    lrclk_bufg => lrclk_bufg_sync2,
+    aclk => aclk,
+    aresetn => aresetn,    
+    config_tdata_o     => config_tdata,
+    config_tready_i     => config_tready,
+    config_tvalid_o      => config_tvalid,
+    tlast_o             => fft_s_tlast
+    );
+    
+counter_left: counter
+    generic map(MAX_COUNT => 2*FIFO_DEPTH)
+    port map(
+    clk_i => lrclk_bufg_sync2,
+    reset_i => reset_l,
+    enable_i => '1',
+    tc_o => tc_l);
+    
+counter_right: counter
+    generic map(MAX_COUNT => 2*FIFO_DEPTH)
+    port map(
+    clk_i => lrclk_bufg_sync2,
+    reset_i => reset_r,
+    enable_i => '1',
+    tc_o => tc_r);
+
 fft_core_inst : axis_fft
     port map (
         aclk                 => aclk,
         aresetn              => aresetn,
 
-        -- Automated configuration (Config Vector = 1 for Forward FFT)
-        s_axis_config_tdata  => std_logic_vector(to_unsigned(1, CONFIG_WIDTH)), 
+        s_axis_config_tdata  => config_tdata, 
         s_axis_config_tvalid => config_tvalid,
         s_axis_config_tready => config_tready,
 
-        -- Input Stream (From Input FIFO output)
+        -- Input Stream 
         s_axis_data_tdata    => fft_s_tdata,
-        s_axis_data_tvalid   => fifo_in_m_tvalid,
-        s_axis_data_tready   => fifo_in_m_tready,
-        s_axis_data_tlast    => fifo_in_m_tlast,
+        s_axis_data_tvalid   => fft_s_tvalid,
+        s_axis_data_tready   => fft_s_tready,
+        s_axis_data_tlast    => fft_s_tlast, --comes from fsm
 
-        -- Output Stream (Directly maps into Output FIFO input)
+        -- Output Stream 
         m_axis_data_tdata    => fft_m_tdata,
         m_axis_data_tvalid   => fft_m_tvalid,
         m_axis_data_tready   => fft_m_tready,
         m_axis_data_tlast    => fft_m_tlast
     );
 
-------------------------------------------------------------------------
--- 5. Output FIFO Instantiation (64-bit Data Width)
-------------------------------------------------------------------------
+
 output_fifo_inst : axis_fifo
     generic map (
-        DATA_WIDTH => OUT_DATA_WIDTH,
+        DATA_WIDTH => DATA_WIDTH,
         FIFO_DEPTH => FIFO_DEPTH
     )
     port map (
         s00_axis_aclk     => aclk,
         s00_axis_aresetn  => aresetn,
         s00_axis_tvalid   => fft_m_tvalid,
-        s00_axis_tdata    => fft_m_tdata,
-        s00_axis_tstrb    => (others => '1'), -- Strobe fully active for complex data
+        s00_axis_tdata    => (fft_m_tdata(23 downto 0) & lrclk_raw_sync2 & "0000000"),
+        s00_axis_tstrb    => (others => '1'),
         s00_axis_tlast    => fft_m_tlast,
         s00_axis_tready   => fft_m_tready,
 
@@ -217,4 +330,93 @@ output_fifo_inst : axis_fifo
         m00_axis_tready   => m_axis_tready
     );
 
-end Structural;
+dbl_ff_sync: process(aclk) --Double FF synchronizer
+begin
+    if rising_edge(aclk) then
+        lrclk_raw_sync1 <= lrclk_raw;
+        lrclk_raw_sync2 <= lrclk_raw_sync1;
+        lrclk_bufg_sync1 <= lrclk_bufg;
+        lrclk_bufg_sync2 <= lrclk_bufg_sync1;        
+    end if;
+end process;
+
+----------------------------------------------------------------------------
+--FSM Next State Logic Process
+
+next_state_logic : process(curr_state, tc_l, tc_r)
+begin
+
+	-- Add default conditions here
+	next_state <= curr_state; 
+    case curr_state is	
+
+        when LeftSelect  =>	
+            if tc_l = '1' then
+                next_state <= RightSelect;
+            end if;
+            
+        when RightSelect  =>	
+            if tc_r = '1' then
+                next_state <= LeftSelect ;
+            end if;
+        
+        when others => 
+            next_state <= LeftSelect ; 
+    end case;
+end process next_state_logic;
+
+-- FSM Output Logic Process
+fsm_output_logic : process(curr_state) 
+begin
+
+	-- Defaults
+	reset_l  <= '0';
+	reset_r <= '0';
+	case curr_state is		
+
+		when LeftSelect  =>		
+	       reset_r <= '1';
+		
+		when RightSelect =>		
+	       reset_l <= '1';
+		
+		when others =>
+	end case;	
+end process fsm_output_logic;
+
+----------------------------------------------------------------------------
+-- 5e. FSM State Update Process (synchronous, clocked)
+state_update : process (lrclk_bufg_sync2)
+begin
+	if (rising_edge(lrclk_bufg_sync2)) then
+		curr_state <= next_state; 
+	end if;
+end process state_update;
+
+--Logic
+
+fifo_l_s_tvalid <= fifo_0_m_tvalid when lrclk_raw_sync2 = '0' else '0';
+fifo_r_s_tvalid <= '0'             when lrclk_raw_sync2 = '0' else fifo_0_m_tvalid;
+
+fifo_l_s_tdata  <= fifo_0_m_tdata;
+fifo_l_s_tstrb  <= fifo_0_m_tstrb;
+fifo_l_s_tlast  <= fifo_0_m_tlast;
+fifo_l_s_tready <= fifo_0_m_tready;
+
+fifo_r_s_tdata  <= fifo_0_m_tdata;
+fifo_r_s_tstrb  <= fifo_0_m_tstrb;
+fifo_r_s_tlast  <= fifo_0_m_tlast;
+fifo_r_s_tready <= fifo_0_m_tready;
+
+
+
+fft_s_tvalid <= fifo_l_m_tvalid when tc_l = '1' else
+                fifo_r_m_tvalid when tc_r = '1' else
+                '0';
+
+fft_s_tdata  <= fifo_l_m_tdata(24 downto 1) & "000000000000000000000000" when tc_l = '1' else
+                fifo_r_m_tdata(24 downto 1) & "000000000000000000000000"   when tc_r = '1' else
+                (others => '0');
+
+    
+end Behavioral;
--- fft_feeder_fsm.vhd
+++ fft_feeder_fsm.vhd
@@ -13,12 +13,125 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity fft_feeder_fsm is
---  Port ( );
+	generic (
+		FIFO_DEPTH	: integer	:= 64;
+		FFT_DATA_WIDTH : integer	:= 48
+	);    
+    Port ( 
+        --Config ports 
+        config_tdata_o : out std_logic_vector(15 downto 0);
+        config_tvalid_o : out std_logic;       
+        config_tready_i : in std_logic;  
+        
+        lrclk_bufg : in std_logic;
+        aclk     : in std_logic;
+		aresetn  : in std_logic;
+
+		tlast_o    : out std_logic
+    );
 end fft_feeder_fsm;
+
 
 architecture Behavioral of fft_feeder_fsm is
 
+signal tc : std_logic := '0';
+signal lrclk_sync1 : std_logic;
+signal lrclk_sync2 : std_logic;
+signal data_o : std_logic_vector(FFT_DATA_WIDTH-1 downto 0);
+
+type state_type is (ResetState, ConfigState, RunState);	
+signal curr_state, next_state : state_type := ResetState;
+
+component counter is
+    Generic ( MAX_COUNT : integer := FIFO_DEPTH);   
+    Port (  clk_i       : in STD_LOGIC;			
+            reset_i     : in STD_LOGIC;		
+            enable_i    : in STD_LOGIC;				
+            tc_o        : out STD_LOGIC);
+end component counter;
+
+
 begin
 
+counter_inst: counter
+    port map(
+    clk_i => lrclk_sync2,
+    reset_i => tc,
+    enable_i => '1',
+    tc_o => tc);
+
+----------------------------------------------------------------------------
+--FSM Next State Logic Process
+
+next_state_logic : process(curr_state, config_tready_i, aresetn, tc)
+begin
+
+	-- Add default conditions here
+	next_state <= curr_state; 
+    if aresetn = '0' then
+        next_state <= ResetState;
+    else
+        case curr_state is	
+    
+            when ResetState =>	
+                next_state <= ConfigState;
+            
+            when ConfigState =>	
+                if (config_tready_i = '1') and (aresetn = '1') then		
+                    next_state <= RunState;
+                end if;
+            
+            when RunState =>
+                if tc = '1' then
+                    next_state <= ConfigState;
+                else next_state <= RunState;
+                end if;
+            
+            when others => -- this is like the "else" part of an if/else statement, but shouldn't reached
+                next_state <= ResetState; -- can put a default here in case something weird happens in the hardware
+                
+        end case;
+    end if;
+end process next_state_logic;
+
+-- FSM Output Logic Process
+fsm_output_logic : process(curr_state) 
+begin
+
+	-- Defaults
+	config_tvalid_o  <= '0';
+	config_tdata_o <= "0000101010101101";
+    tlast_o <= '0';
+	case curr_state is		
+
+		when ResetState =>		
+	       config_tdata_o <= "0000000000000000";  
+		
+		when ConfigState =>		
+			config_tvalid_o  <= '1';	
+		    tlast_o <= '1';
+		    
+		when RunState =>
+		
+		when others =>
+	end case;	
+end process fsm_output_logic;
+
+----------------------------------------------------------------------------
+-- 5e. FSM State Update Process (synchronous, clocked)
+state_update : process (aclk)
+begin
+	if (rising_edge(aclk)) then
+		curr_state <= next_state; 
+	end if;
+end process state_update;
+
+dbl_ff_sync: process(aclk) --Double FF synchronizer
+begin
+    if rising_edge(aclk) then
+        lrclk_sync1 <= lrclk_bufg;
+        lrclk_sync2 <= lrclk_sync1;
+    end if;
+end process;
 
 end Behavioral;
